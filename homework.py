d20f0e2792b81ae9a2759fbd7394abc316a2e1db
Никита Федяев <itsfedos@gmail.com>
2022-08-22 23:48:14 +0300

    for 3rd review

--- homework.py
+++ homework.py
@@ -22,6 +22,8 @@ RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
+BASEDIR = os.path.dirname('')
+
 VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
@@ -33,13 +35,14 @@ logging.basicConfig(
     filename='logs_bot.log',
     format='%(asctime)s - %(funcName)s - %(lineno)s - '
            '%(levelname)s - %(name)s - %(message)s',
+    handlers=[
+        logging.StreamHandler(stream=sys.stdout),
+        logging.FileHandler(
+            filename=BASEDIR + 'logs_bot.log',
+            encoding='UTF-8'
+        )
+    ]
 )
-
-logger = logging.getLogger(__name__)
-handlers = [
-    logging.StreamHandler(stream=sys.stdout),
-    logging.FileHandler('logs_bot.log')
-]
 
 
 def check_tokens():
@@ -50,8 +53,8 @@ def check_tokens():
 def send_message(bot, message):
     """Отправляет сообщение в TG чат."""
     try:
-        bot.send_message(TELEGRAM_CHAT_ID, message)
         logging.info('Начата отправка сообщения в телеграмм')
+        bot.send_message(TELEGRAM_CHAT_ID, message)
     except TelegramError as error:
         logging.error(f'Сообщение не отправлено! {error}')
     else:
@@ -67,7 +70,9 @@ def get_api_answer(current_timestamp):
         'headers': HEADERS,
         'params': params
     }
-    logging.info('Начался запрос к API. Аргументы передаются из словаря.')
+    logging.info(('Начался запрос к API. {url}, {headers}, {params}.')
+                 .format(url=pargs['url'], headers=pargs['headers'],
+                         params=pargs['params']))
     try:
         response = requests.get(**pargs)
         if response.status_code != HTTPStatus.OK:
@@ -78,7 +83,10 @@ def get_api_answer(current_timestamp):
         logging.info('Данные по API успешно получены')
         return response.json()
     except Exception as error:
-        raise ConnectionError(f'{error} {pargs}')
+        raise ConnectionError(('{error}: {url}, {headers}, {params}.')
+                              .format(error=error, url=pargs['url'],
+                                      headers=pargs['headers'],
+                                      params=pargs['params']))
 
 
 def check_response(response):
@@ -104,10 +112,11 @@ def parse_status(homework):
     homework_status = homework.get('status')
     if homework_status not in VERDICTS:
         raise ValueError('Получен неизвестный статус работы')
-    verdict = VERDICTS[homework_status]
     return (
-        f'Изменился статус проверки работы "{homework_name}". '
-        f'{verdict}'.format(homework_name=homework_name, verdict=verdict)
+        'Изменился статус проверки работы "{homework_name}". {verdict}'
+            .format(
+            homework_name=homework_name, verdict=VERDICTS[homework_status]
+        )
     )
 
 
@@ -118,39 +127,39 @@ def main():
         raise custom.TokenError('Проблема с получением токенов!')
     current_timestamp = int(time.time())
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    current_report = dict(message=message)
+    prev_report = {}
 
     while True:
         try:
             response = get_api_answer(current_timestamp)
             homework = check_response(response)
             message = parse_status(homework)
-            current_report = {
-                'message': message
-            }
-            prev_report = {}
             if current_report != prev_report:
-                logging.info('Отправка сообщения о новом статусе')
                 send_message(bot, message)
+                prev_report = current_report.copy()
+                current_timestamp = response.get(
+                    'current_date',
+                    current_timestamp
+                )
             else:
                 logging.info('Нет обновления статуса')
-                raise custom.NotForReply('Нет новых сообщений')
-            prev_report = current_report.copy()
-            current_timestamp = response.get('current_date', current_timestamp)
+        except custom.EmptyResponseFromAPI as error:
+            logging.error(f'{error}. Пустой ответ от API.')
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            current_report = {
-                'message': message
-            }
+            current_report = dict(message=message)
             prev_report = {}
             if current_report != prev_report:
-                logging.error(f'Отправка сообщения о {error}')
+                logging.exception(f'Отправка сообщения о {error}')
                 send_message(bot, message)
-            else:
-                logging.error('Нет новых сообщений. Сбой.')
-                raise custom.NotForReply('Нет обновлений. Сбой.')
+                prev_report = current_report.copy()
+                current_timestamp = response.get(
+                    'current_date',
+                    current_timestamp
+                )
         finally:
             time.sleep(RETRY_TIME)
-        return
 
 
 if __name__ == '__main__':
